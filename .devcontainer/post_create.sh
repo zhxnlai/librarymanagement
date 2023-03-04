d8e5db71aba35f4146d752fc32dcc82a2ef3c456
Zhixuan Lai <zhixuan@tryforest.com>
2023-03-04 11:42:18 -0800

    Install app

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,6 +6,7 @@ bench new-site erp.localhost --mariadb-root-password 123 --admin-password 123 --
 bench --site erp.localhost set-config developer_mode 1
 bench --site erp.localhost clear-cache
 bench --site erp.localhost install-app erpnext
+bench --site erp.localhost install-app librarymanagement
 
 # Make this the default site.
 bench use erp.localhost
